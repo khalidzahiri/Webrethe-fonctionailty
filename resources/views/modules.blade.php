9e8a0ffdbd01805be76b9f4363e8716521d67efa
khalidzahiri <virtues01seasons@icloud.com>
2022-09-14 17:19:21 +0200

    changement de données

--- resources/views/modules.blade.php
+++ resources/views/modules.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
-    <p> je suis la page des modules</p>
-    @if($modules->count() > 0)
+    <p> les modules activées Sont: </p>
+    @if($modules->count() > 0 && $modules->is_active=1)
         @foreach($modules as $module)
             <h2><a href="{{ route('module.show', ['id'=> $module ->id]) }}">{{ $module->name }}</a></h2>
         @endforeach
